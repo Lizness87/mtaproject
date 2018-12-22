e8a6f01ef692ebb88e63ae02cbe90c06e14550e6
Elizabeth Miller <elizabethmiller.lm@gmail.com>
2018-12-22 15:26:18 +0200

    Added homepage for parent and assistant

--- controllers/authenticate-controller.js
+++ controllers/authenticate-controller.js
@@ -1,34 +1,51 @@
 connection = require('./../config');
+express = require('express');
+var app = express();
 module.exports.authenticate=function(req,res){
-    var username=req.body.uname;
-    var password=req.body.psw;
-    connection.query('SELECT * FROM users WHERE username = ?',[username], function (error, results, fields) {
-      if (error) {
-          res.json({
-            status:false,
-            message:'There are some errors with query'
-            })
-      }else{
-        if(results.length >0){
-            if(password==results[0].password){
-                res.json({
-                    status:true,
-                    message:'Successfully authenticated'
-                })
-            }else{
-                res.json({
-                  status:false,
-                  message:"Username and password does not match"
-                 });
-            }
-         
-        }
-        else{
-          res.json({
-              status:false,    
-            message:"Username does not exist"
-          });
-        }
-      }
+var username=req.body.uname;
+var password=req.body.psw;
+
+
+app.use(express.static('assets'))
+
+
+  connection.query('SELECT * FROM users WHERE username = ?',[username], function (error, user_result, fields) {
+  if (error) {
+    // console.log("error ocurred",error);
+    res.send({
+      "code":400,
+      "failed":"error ocurred"
     });
+  }else{
+    // console.log('The solution is: ', results);
+    if(user_result.length >0){
+         if(user_result[0].password == password){
+         console.log(JSON.stringify(user_result[0]));
+         if (user_result[0].privilege=='parent') {
+         res.redirect('/login/parent');
+         }
+         else if (user_result[0].privilege=='manager') {
+         res.redirect('/login/manager');
+         }
+         else if (user_result[0].privilege='assistant') {
+         res.redirect('/login/assistant');
+        }
+        
+    }else{
+      res.send({
+      "code":204,
+      "success":"Username and password does not match"
+      });
+    }
+  }
+
+
+    else{
+      res.send({
+        "code":204,
+        "success":"Username does not exist"
+          });
+    }
+    }
+  });
 }

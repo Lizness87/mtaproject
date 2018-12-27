c323aa7e2396320b3c255e80a78bcf716ab37eb6
Liz Miller <liz.miller@pb.com>
2018-12-27 02:34:47 +0200

    Changed registration forms to ejs

--- controllers/authenticate-controller.js
+++ controllers/authenticate-controller.js
@@ -4,53 +4,50 @@ connection = require('./../config');
 express = require('express');
 var app = express();
 module.exports.authenticate=function(req,res){
-var username=req.body.uname;
-var password=req.body.psw;
+    var username=req.body.uname;
+    var password=req.body.psw;
 
 
-app.use(express.static('assets'))
-
-
-  connection.query('SELECT * FROM users WHERE username = ?',[username], function (error, user_result, fields) {
-  if (error) {
-    // console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
+    app.use(express.static('assets'));
+    connection.query('SELECT * FROM users WHERE username = ?',[username], function (error, user_result, fields) {
+       if (error) {
+            // console.log("error ocurred",error);
+            res.send({
+               "code":400,
+              "failed":"error ocurred"
+           });
+        }else{
+             // console.log('The solution is: ', results);
+             if(user_result.length >0){
+                if(user_result[0].password == password){
+                    console.log(JSON.stringify(user_result[0]));
+                    if (user_result[0].privilege=='parent') {
+                       var data={uid:user_result[0].uid};
+                        res.render('pages/homepageparent', data);
+                    }
+                    else if (user_result[0].privilege=='manager') {
+                         var data={uid:user_result[0].uid};
+                        res.render('pages/homepagemanager', data);
+                   }
+                    else if (user_result[0].privilege='assistant') {
+                         var data={uid:user_result[0].uid};
+                   }
+    
+                }else{
+                    res.send({
+                       "code":204,
+                         "success":"Username and password does not match"
+                    });
+                 }
+             }
+    
+    
+             else{
+                 res.send({
+                    "code":204,
+                    "success":"Username does not exist"
+               });
+            }
+         }
     });
-  }else{
-    // console.log('The solution is: ', results);
-    if(user_result.length >0){
-         if(user_result[0].password == password){
-         console.log(JSON.stringify(user_result[0]));
-         if (user_result[0].privilege=='parent') {
-             var data={uid:user_result[0].uid};
-	           res.render('pages/homepageparent', data);
-         }
-         else if (user_result[0].privilege=='manager') {
-	         var data={uid:user_result[0].uid};
-           res.render('pages/homepagemanager', data);
-         }
-         else if (user_result[0].privilege='assistant') {
-          var data={uid:user_result[0].uid};
-	        res.render('pages/homepageassistant', data);
-        }
-        
-    }else{
-      res.send({
-      "code":204,
-      "success":"Username and password does not match"
-      });
-    }
-  }
-
-
-    else{
-      res.send({
-        "code":204,
-        "success":"Username does not exist"
-          });
-    }
-    }
-  });
-}
+};

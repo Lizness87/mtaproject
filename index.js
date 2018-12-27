000863a1dfcd944240421fb852cb23842bea0b08
Liz Miller <liz.miller@pb.com>
2018-12-27 02:36:53 +0200

    Changed registration forms to ejs files and routes

--- index.js
+++ index.js
@@ -8,54 +8,61 @@ var app = express();
 
 //set the view engine to ejs
 app.set('view engine', 'ejs');
- 
+
 var authenticateController=require('./controllers/authenticate-controller');
- 
+var registrationController=require('./controllers/registration-controller');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('assets'))
+app.use(express.static('assets'));
 
 //Redirect homepage to login
 
-app.get('/', function (req, res) {  
-res.redirect('/login');
-})
+app.get('/', function (req, res) {
+    res.redirect('/login');
+});
 
 //Load login.html after redirecting to login
 
-app.get('/login', function (req, res) {  
+app.get('/login', function (req, res) {
 
-	res.render('pages/login');  
+    res.render('pages/login');
 
-})
+});
 
 
 //Load parent homepage after authentication as a parent
 
 app.get('/login/parent', function (req, res) {
-	res.render('pages/homepageparent');
-})
+    res.render('pages/homepageparent');
+});
 
 
 //Load manager homepage after authentication as a manager
 
 app.get('/login/manager', function (req, res) {
-	res.render('pages/homepagemanager' );
-})
+    res.render('pages/homepagemanager' );
+});
 
 
 
 //Load assistant homepage after authentication as an assistant
 
 app.get('/login/assistant', function (req, res) {
-	res.render('pages/homepageassistant' );
-})
+    res.render('pages/homepageassistant' );
+});
 
 
 /* route to handle login */
 app.post('/api/authenticate',authenticateController.authenticate);
- 
+
+/* routes to handle registration */
+app.get('/registration1',registrationController.registration1);
+app.get('/registration2',registrationController.registration2);
+app.get('/registration3',registrationController.registration3);
+app.get('/registration4',registrationController.registration4);
+
 console.log(authenticateController);
 app.post('/controllers/authenticate-controller', authenticateController.authenticate);
 app.listen(5000);
